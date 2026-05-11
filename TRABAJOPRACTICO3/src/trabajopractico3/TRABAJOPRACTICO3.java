eb212e46468bd376b35d9fa75f7653376ff79bd3
Pc <Pc@192.168.0.105>
2026-05-11 18:24:31 -0300

    Para enviar prueba

--- TRABAJOPRACTICO3/src/trabajopractico3/TRABAJOPRACTICO3.java
+++ TRABAJOPRACTICO3/src/trabajopractico3/TRABAJOPRACTICO3.java
@@ -15,6 +15,7 @@ public class TRABAJOPRACTICO3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    System.out.println("Hola Mundo");
     }
     
 }
